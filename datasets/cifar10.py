cb52671765c98b8be7fdbcf7b1ac9206f27a7e76
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-09 22:18:26 +0800

    Create cifar10.py

---
+++ datasets/cifar10.py
@@ -0,0 +1,15 @@
+from .Packer import Packer
+import tensorflow.keras.datasets as ds
+
+
+class cifar10(Packer):
+
+  def __init__(self):
+    super(cifar10, self).__init__()
+    self.NUM_TRAIN = 50000
+    self.NUM_TEST = 10000
+    self.NUM_CLASSES = 10
+    self.IMAGE_SIZE = 32
+    self.IMAGE_DEPTH = 3
+    (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.cifar10.load_data()
+    self.train_images, self.test_images = self.train_images / 255.0, self.test_images / 255.0
