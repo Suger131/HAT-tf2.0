800843bdc5588373b2eb115aa590184482fd2cec
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-12 18:59:29 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,8 @@
   models 子包的init文件
 '''
 
+from .Model import *
+
 from .MLP import MLP
 from .LeNet import LeNet
 from .VGG10 import VGG10
@@ -9,3 +11,4 @@ from .NVGG10 import NVGG10
 from .David import David
 from .GoogleNet import GoogleNet
 from .WRN_28_10 import WRN_28_10
+from .MobileNet import MobileNet
