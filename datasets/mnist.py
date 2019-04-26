ce7c82e4a560de783de68dacce9e883db7db777c
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-26 15:33:18 +0800

    Update mnist.py

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -9,18 +9,13 @@ class mnist(Packer):
     self.NUM_TRAIN = 60000
     self.NUM_TEST = 10000
     self.NUM_CLASSES = 10
-    self.IMAGE_SIZE = 28
-    self.IMAGE_DEPTH = 1
+    self.IMAGE_SHAPE = (28, 28, 1)
     (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.mnist.load_data()
     self.train_images, self.test_images = self.train_images / 255.0, self.test_images / 255.0
-    self.train_images = self.train_images.reshape((self.NUM_TRAIN, self.IMAGE_SIZE, self.IMAGE_SIZE, self.IMAGE_DEPTH))
-    self.test_images = self.test_images.reshape((self.NUM_TEST, self.IMAGE_SIZE, self.IMAGE_SIZE, self.IMAGE_DEPTH))
+    self.train_images = self.train_images.reshape((self.NUM_TRAIN, *self.IMAGE_SHAPE))
+    self.test_images = self.test_images.reshape((self.NUM_TEST, *self.IMAGE_SHAPE))
 
 # test mode
 if __name__ == "__main__":
-  class Args:
-    pass
-  a = Args()
   m = mnist()
-  m.ginfo(a)
-  print(a.__dict__)
+  print(m.ginfo())
