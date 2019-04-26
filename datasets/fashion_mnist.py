1c9305c46b5a1d4edf56adc745375dde1189c1f1
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-26 15:33:14 +0800

    Update fashion_mnist.py

--- datasets/fashion_mnist.py
+++ datasets/fashion_mnist.py
@@ -9,10 +9,9 @@ class fashion_mnist(Packer):
     self.NUM_TRAIN = 60000
     self.NUM_TEST = 10000
     self.NUM_CLASSES = 10
-    self.IMAGE_SIZE = 28
-    self.IMAGE_DEPTH = 1
+    self.IMAGE_SHAPE = (28, 28, 1)
     (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.fashion_mnist.load_data()
     self.train_images, self.test_images = self.train_images / 255.0, self.test_images / 255.0
-    self.train_images = self.train_images.reshape((self.NUM_TRAIN, self.IMAGE_SIZE, self.IMAGE_SIZE, self.IMAGE_DEPTH))
-    self.test_images = self.test_images.reshape((self.NUM_TEST, self.IMAGE_SIZE, self.IMAGE_SIZE, self.IMAGE_DEPTH))
+    self.train_images = self.train_images.reshape((self.NUM_TRAIN, *self.IMAGE_SHAPE))
+    self.test_images = self.test_images.reshape((self.NUM_TEST, *self.IMAGE_SHAPE))
     
