f8f4096f58aadfdfdd0c14b8b6cbe076e53571ed
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-26 15:33:09 +0800

    Update cifar10.py

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -9,7 +9,6 @@ class cifar10(Packer):
     self.NUM_TRAIN = 50000
     self.NUM_TEST = 10000
     self.NUM_CLASSES = 10
-    self.IMAGE_SIZE = 32
-    self.IMAGE_DEPTH = 3
+    self.IMAGE_SHAPE = (32, 32, 3)
     (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.cifar10.load_data()
     self.train_images, self.test_images = self.train_images / 255.0, self.test_images / 255.0
