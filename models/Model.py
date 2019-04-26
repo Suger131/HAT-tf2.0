e69cea553aad1fd59415dd8e22b3d49d479924cb
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-04-26 15:33:34 +0800

    Update Model.py

--- models/Model.py
+++ models/Model.py
@@ -2,14 +2,20 @@ from tensorflow.python.keras.models import load_model
 
 class BasicModel:
 
-  def __init__(self, Args):
-    self.ARGS = Args
+  def __init__(self):
+    self.BATCH_SIZE = 0
+    self.EPOCHS = 0
+    self.OPT = None
+    self.OPT_EXIST = False
 
-  def check_save(self):
-    if self.ARGS:
-      self.model = self.ARGS.SAVE_EXIST and load_model(self.ARGS.LOAD_NAME) or self.model
+  # def check_save(self):
+  #   if self.ARGS:
+  #     self.model = self.ARGS.SAVE_EXIST and load_model(self.ARGS.LOAD_NAME) or self.model
+
+  # def ginfo(self):
+  #   for i in self.__dict__:
+  #     if i in ['BATCH_SIZE', 'EPOCHS', 'OPT', 'OPT_EXIST']:
+  #       self.ARGS.__dict__[i] = self.ARGS.__dict__[i] or self.__dict__[i]
 
   def ginfo(self):
-    for i in self.__dict__:
-      if i in ['BATCH_SIZE', 'EPOCHS', 'OPT', 'OPT_EXIST']:
-        self.ARGS.__dict__[i] = self.ARGS.__dict__[i] or self.__dict__[i]
+    return {i: self.__dict__[i] for i in ['BATCH_SIZE', 'EPOCHS', 'OPT', 'OPT_EXIST']}
