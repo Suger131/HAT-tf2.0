abc5272e77eb1e9d6068a79c2d8ca0f100918a16
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-02 16:20:04 +0800

    Update fashion_mnist.py

--- datasets/fashion_mnist.py
+++ datasets/fashion_mnist.py
@@ -1,16 +1,19 @@
-from .Packer import Packer
+from .Dataset import Dataset
 import tensorflow.keras.datasets as ds
 
 
-class fashion_mnist(Packer):
+class f_mnist(Dataset):
+  """
+  fashion_mnist 数据集
+  """
 
-  def __init__(self):
-    super(fashion_mnist, self).__init__()
+  def args(self):
+    self._MISSION_LIST = ['classfication']
     self.NUM_TRAIN = 60000
     self.NUM_TEST = 10000
     self.NUM_CLASSES = 10
     self.IMAGE_SHAPE = (28, 28, 1)
-    (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.fashion_mnist.load_data()
+    (self.train_images, self.train_labels), (self.test_images, self.test_labels) = ds.mnist.load_data()
     self.train_images, self.test_images = self.train_images / 255.0, self.test_images / 255.0
     self.train_images = self.train_images.reshape((self.NUM_TRAIN, *self.IMAGE_SHAPE))
     self.test_images = self.test_images.reshape((self.NUM_TEST, *self.IMAGE_SHAPE))
