1caa04c447c462d3cd57d1f9bc0f183e46777c1b
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-04 16:06:12 +0800

    Update vgg19.py

--- models/vgg19.py
+++ models/vgg19.py
@@ -1,14 +1,15 @@
 '''
   VGG19 模型
   本模型默认总参数量[参考基准：cifar10]：
-    Total params:           37,716,930
-    Trainable params:       37,708,482
-    Non-trainable params:   8,448
+    Total params:           42,592,962
+    Trainable params:       42,582,210
+    Non-trainable params:   10,752
   本模型默认总参数量[参考基准：ImageNet]：
-    Total params:           138,380,226
-    Trainable params:       138,371,778
-    Non-trainable params:   8,448
+    Total params:           143,256,258
+    Trainable params:       143,245,506
+    Non-trainable params:   10,752
 '''
+
 
 from models.network import NetWork
 from models.advanced import AdvNet
@@ -26,8 +27,7 @@ class vgg19(NetWork, AdvNet):
     self.CONV = [64, 128, 256, 512, 512]
 
     self.LOCAL = [4096, 4096, 1000]
-    self.DROP = [0.3, 0.3, 0]
-    self.LOCAL_LIST = list(zip(self.LOCAL, self.DROP))
+    self.DROP = 0.3
 
     # for test
     # self.BATCH_SIZE = 128
@@ -36,35 +36,34 @@ class vgg19(NetWork, AdvNet):
     # self.OPT_EXIST = True
 
   def build_model(self):
-    x_in = Input(shape=self.IMAGE_SHAPE)
+    x_in = self.input(self.IMAGE_SHAPE)
 
     # 卷积部分
     x = x_in
-    for i in range(self.NUM_LAYERS[0]): x = self.conv_bn(x, self.CONV[0], 3)
-    x = MaxPool2D(2, padding='same')(x)
-    for i in range(self.NUM_LAYERS[1]): x = self.conv_bn(x, self.CONV[1], 3)
-    x = MaxPool2D(2, padding='same')(x)
-    for i in range(self.NUM_LAYERS[2]): x = self.conv_bn(x, self.CONV[2], 3)
-    x = MaxPool2D(2, padding='same')(x)
-    for i in range(self.NUM_LAYERS[3]): x = self.conv_bn(x, self.CONV[3], 3)
-    x = MaxPool2D(self.IMAGE_SHAPE[0] // 16 >= 4 and 2 or 1, padding='same')(x)
-    for i in range(self.NUM_LAYERS[4]): x = self.conv_bn(x, self.CONV[4], 3)
-    if self.IMAGE_SHAPE[0] // 32 >= 4:
-      x = MaxPool2D(2, padding='same')(x)
-    else:
-      x = GlobalAvgPool2D()(x)
-    
+    x = self.repeat(self.conv_bn, self.NUM_LAYERS[0], x, self.CONV[0], 3)
+    x = self.maxpool(x)
+    x = self.repeat(self.conv_bn, self.NUM_LAYERS[1], x, self.CONV[1], 3)
+    x = self.maxpool(x)
+    x = self.repeat(self.conv_bn, self.NUM_LAYERS[2], x, self.CONV[2], 3)
+    x = self.maxpool(x)
+    x = self.repeat(self.conv_bn, self.NUM_LAYERS[3], x, self.CONV[3], 3)
+    x = self.maxpool(x, 2 if self.IMAGE_SHAPE[0] // 16 >= 4 else 1)
+    x = self.repeat(self.conv_bn, self.NUM_LAYERS[4], x, self.CONV[4], 3) 
+    x = self.maxpool(x) if self.IMAGE_SHAPE[0] // 32 >= 4 else self.GAPool(x)
+
     # 全连接部分
-    x = Flatten()(x)
-    for i in self.LOCAL_LIST:
-      x = Dense(i[0], activation='relu')(x)
-      x = Dropout(i[1])(x)
-    x = Dense(self.NUM_CLASSES, activation='softmax')(x)
+    x = self.flatten(x)
+    x = self.local(x, self.LOCAL[0])
+    x = self.dropout(x, self.DROP)
+    x = self.local(x, self.LOCAL[1])
+    x = self.dropout(x, self.DROP)
+    x = self.local(x, self.LOCAL[2])
+    x = self.local(x, self.NUM_CLASSES, activation='softmax')
 
     self.model = Model(inputs=x_in, outputs=x, name='vgg19')
 
 # test part
 if __name__ == "__main__":
-  mod = vgg19(DATAINFO={'IMAGE_SHAPE': (224, 224, 3), 'NUM_CLASSES': 10})
+  mod = vgg19(DATAINFO={'IMAGE_SHAPE': (32, 32, 3), 'NUM_CLASSES': 10})
   print(mod.IMAGE_SHAPE)
   print(mod.model.summary())
