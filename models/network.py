93fdd5ed0604681a20d12fee5096692cc610295e
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-05 16:50:00 +0800

    Update network.py

--- models/network.py
+++ models/network.py
@@ -15,22 +15,43 @@ class NetWork(object):
   """
 
   def __init__(self, *args, **kwargs):
-    self.kwargs = kwargs
+    
+    self._kwargs = kwargs
+    self._default_list = ['BATCH_SIZE', 'EPOCHS', 'OPT', 'OPT_EXIST']
+    self._default_dict = {}
+    self._dict = {}
     self._check_kwargs()
+
+    self._built = True
     self.BATCH_SIZE = 0
     self.EPOCHS = 0
     self.OPT = None
     self.OPT_EXIST = False
     self.args()
+    self._built = False
+
     self.build_model()
+
+  # built-in method
+
+  def __setattr__(self, name, value):
+    if '_built' not in self.__dict__:
+      return super().__setattr__(name, value)
+    if name == '_built':
+      return super().__setattr__(name, value)
+    if self._built:
+      if name in self._default_list:
+        self._default_dict[name] = value
+      else:
+        self._dict[name] = value
+    return super().__setattr__(name, value)
 
   # private method
 
   def _check_kwargs(self):
-    if 'DATAINFO' in self.kwargs:
-      self.__dict__ = {**self.__dict__, **self.kwargs['DATAINFO']}
-      self.kwargs.pop('DATAINFO')
-    self.__dict__ = {**self.__dict__, **self.kwargs}
+    if 'DATAINFO' in self._kwargs:
+      self.__dict__ = {**self.__dict__, **self._kwargs.pop('DATAINFO')}
+    self.__dict__ = {**self.__dict__, **self._kwargs}
 
   def args(self):
     pass
@@ -41,4 +62,4 @@ class NetWork(object):
   # public method
 
   def ginfo(self):
-    return {i: self.__dict__[i] for i in ['BATCH_SIZE', 'EPOCHS', 'OPT', 'OPT_EXIST']}
+    return self._default_dict, self._dict
