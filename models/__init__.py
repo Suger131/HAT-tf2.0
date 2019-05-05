4898c6dc02c668d94e7b3150c5457d3a4c44702c
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-06 07:42:12 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -7,4 +7,5 @@ from models.vgg16 import vgg16
 from models.vgg19 import vgg19
 from models.nvgg16 import nvgg16
 from models.resnet101 import resnet101
+from models.densenet39 import densenet39
 from models.wrn28_10 import wrn28_10
