2de3e8e85c5b43c972e65d44c5b3a96549c2a55a
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-05 16:50:05 +0800

    Update Arg.py

--- Arg.py
+++ Arg.py
@@ -1,5 +1,4 @@
 import os
-import time
 
 import tensorflow as tf
 from tensorflow.python.keras.models import load_model
@@ -21,11 +20,15 @@ from datasets import *
 from models import *
 
 
-class Args:
+class Args(object):
 
   def __init__(self):
-
-    self.START_TIME = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+    # built-in args
+    self._paramc = []
+    self._logc = []
+    self._warning_args = []
+    self._dir_list = []
+    # envs args
     self.IS_TRAIN = True
     self.IS_TEST = True
     self.IS_SAVE = True
@@ -40,24 +43,20 @@ class Args:
     self.OPT = None
     self.OPT_EXIST = False
     self.LOSS_MODE = None
-    self.WARNING_ARGS = []
-    self.DIR_LIST = []
+    # build
     self.IN_ARGS = input('=>').split(' ')
+    self._Log = Log(log_dir='logs/logger')
+    self._timer = Timer(self._Log)
+    self._config = None
     self.user()
-    self.Log = Log(log_dir='logs/logger')
+    self._input_processing()
+    self._envs_processing()
+    # built
 
-    self.input_processing()
-    self.envs_processing()
-
-  @property
-  def _time(self):
-    return time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
-
-  def _mktime(self, timex):
-    return time.mktime(time.strptime(timex, '%Y-%m-%d-%H-%M-%S'))
+  # private method
 
   def _error(self, args, text):
-    self.Log(args, _T=text, _A='Error')
+    self._Log(args, _T=text, _A='Error')
     os._exit(1)
 
   def _check_args(self, item, lists, args_name, data='', isdigit=False):
@@ -81,7 +80,15 @@ class Args:
         except:
           self.__dict__[i] = dicts[i]
 
-  def input_processing(self):
+  def _write_config(self):
+    self._config = Config(f"{self.SAVE_DIR}/config")
+    if not self._config.if_param():
+      for di in self._paramc:
+        self._config.param(di)
+    for di in self._logc:
+      self._config.log(di)
+
+  def _input_processing(self):
 
     for i in self.IN_ARGS:
 
@@ -96,7 +103,7 @@ class Args:
             self._check_args(temp[0], ['batch_size', 'bat'], 'BATCH_SIZE', temp[1], isdigit=True) or
             self._check_args(temp[0], ['mode'], 'RUN_MODE', temp[1])): pass
         else:
-          self.WARNING_ARGS.append(temp[0])
+          self._warning_args.append(temp[0])
 
       elif (self._check_args(i, ['gimage', 'gimg'], 'RUN_MODE', data='gimage') or
             self._check_args(i, ['no-gimage', 'n-gimg'], 'RUN_MODE', data='no-gimage') or
@@ -104,10 +111,10 @@ class Args:
             self._check_args(i, ['test-only', 'test-o', 'test'], 'RUN_MODE', data='test')): pass
 
       else:
-        self.WARNING_ARGS.append(i)
-    self.Log(self.WARNING_ARGS, _A='Warning', text='Unsupported option:')
+        self._warning_args.append(i)
+    self._Log(self._warning_args, _A='Warning', text='Unsupported option:')
 
-  def envs_processing(self):
+  def _envs_processing(self):
 
     # load user datasets & models (don't cover)
     self._get_args(self.USER_DICT_N)
@@ -117,8 +124,8 @@ class Args:
     self.H5_NAME = f'{self.SAVE_DIR}\{self.DATASETS_NAME}_{self.MODELS_NAME}'
     
     # make dir
-    self.DIR_LIST.extend([self.SAVE_DIR])
-    for i in self.DIR_LIST:
+    self._dir_list.extend([self.SAVE_DIR])
+    for i in self._dir_list:
       if not os.path.exists(i):
         os.makedirs(i)
 
@@ -143,17 +150,21 @@ class Args:
 
     # get dataset object
     call_dataset = globals().get(self.DATASETS_NAME)
-    callable(call_dataset) and self.Log(self.DATASETS_NAME, _T='Loading Dataset:') or self._error(self.DATASETS_NAME, 'Not in Datasets:')
+    callable(call_dataset) and self._Log(self.DATASETS_NAME, _T='Loading Dataset:') or self._error(self.DATASETS_NAME, 'Not in Datasets:')
     self.DATASET = call_dataset()
-    self._get_args(self.DATASET.ginfo())
-    self.Log(self.DATASETS_NAME, _T='Loaded Dataset:')
+    _dataset = self.DATASET.ginfo()
+    self._get_args(_dataset[0])
+    self._paramc.append(_dataset[1])
+    self._Log(self.DATASETS_NAME, _T='Loaded Dataset:')
 
     # get model object
     call_model = globals().get(self.MODELS_NAME)
-    callable(call_model) and self.Log(self.MODELS_NAME, _T='Loading Model:') or self._error(self.MODELS_NAME, 'Not in Models:')
+    callable(call_model) and self._Log(self.MODELS_NAME, _T='Loading Model:') or self._error(self.MODELS_NAME, 'Not in Models:')
     self.MODEL = call_model(DATAINFO=self.DATAINFO)
-    self._get_args(self.MODEL.ginfo())
-    self.Log(self.MODELS_NAME, _T='Loaded Model:')
+    _model = self.MODEL.ginfo()
+    self._get_args(_model[0])
+    self._paramc.extend(_model)
+    self._Log(self.MODELS_NAME, _T='Loaded Model:')
 
     # load user args (don't cover)
     self._get_args(self.USER_DICT)
@@ -161,40 +172,42 @@ class Args:
     # mode envs
     if self.RUN_MODE == 'no-gimage':
       self.IS_GIMAGE = False
-      self.Log('Not get model image.')
+      self._Log('Not get model image.')
     if self.RUN_MODE == 'gimage':
       self.IS_TRAIN = False
       self.IS_TEST = False
       self.IS_SAVE = False
-      self.Log('Get model image only.')
+      self._Log('Get model image only.')
     elif self.RUN_MODE == 'train':
       self.IS_TEST = False
-      self.Log('train only.')
+      self._Log('train only.')
     elif self.RUN_MODE == 'test':
       self.IS_TRAIN = False
       self.IS_SAVE = False
-      self.Log('test only.')
+      self._Log('test only.')
 
     # log some mode info
     if self.RUN_MODE not in ['gimage']:
-      self.Log(self.EPOCHS, _T='Epochs:')
-      self.Log(self.BATCH_SIZE, _T='Batch size:')
-      self.Log('', _L=['Model Optimizer exist.', f'Using Optimizer: {self.OPT}'], _B=self.OPT_EXIST)
+      self._Log(self.EPOCHS, _T='Epochs:')
+      self._Log(self.BATCH_SIZE, _T='Batch size:')
+      self._Log('', _L=['Model Optimizer exist.', f'Using Optimizer: {self.OPT}'], _B=self.OPT_EXIST)
       if self.RUN_MODE == 'test':
-        self.Log('', _L=['h5 exist.', 'h5 not exist, testing a fresh model.'], _B=self.SAVE_EXIST)
+        self._Log('', _L=['h5 exist.', 'h5 not exist, testing a fresh model.'], _B=self.SAVE_EXIST)
       else:
-        self.Log('', _L=['h5 exist.', 'h5 not exist, create one.'], _B=self.SAVE_EXIST)
-      self.Log(self.LOG_DIR + '\\', _T='logs dir:')
+        self._Log('', _L=['h5 exist.', 'h5 not exist, create one.'], _B=self.SAVE_EXIST)
+      self._Log(self.LOG_DIR + '\\', _T='logs dir:')
 
     # check save
-    self.SAVE_EXIST and self.Log(self.LOAD_NAME ,_T='Loading h5:')
+    self.SAVE_EXIST and self._Log(self.LOAD_NAME ,_T='Loading h5:')
     self.MODEL.model = self.SAVE_EXIST and load_model(self.LOAD_NAME) or self.MODEL.model
-    self.SAVE_EXIST and self.Log(self.LOAD_NAME ,_T='Loaded h5:')
+    self.SAVE_EXIST and self._Log(self.LOAD_NAME ,_T='Loaded h5:')
 
     # compile model
     self.MODEL.model.compile(optimizer=self.OPT,
                              loss=self.LOSS_MODE,
                              metrics=['accuracy'])
+
+  # public method
 
   def train(self):
     
@@ -206,37 +219,27 @@ class Args:
                                        write_graph=True,
                                        write_images=True)
 
-    self.START_TIME = self._time
-
-    self.Log(self.START_TIME, _T='Start training:')
-
-    self.MODEL.model.fit(self.DATASET.train_images,
-                         self.DATASET.train_labels,
-                         epochs=self.EPOCHS,
-                         batch_size=self.BATCH_SIZE,
-                         validation_data=(self.DATASET.test_images, self.DATASET.test_labels), 
-                         callbacks=[tensorboard_callback])
-
-    self.STOP_TIME = self._time
-    self.Log(self.STOP_TIME, _T='Stop training:')
-    self.TRAIN_COST_TIME = self._mktime(self.STOP_TIME) - self._mktime(self.START_TIME)
-    self.Log(self.TRAIN_COST_TIME, _T='Train time (second):')
+    _, result = self._timer.timer('train', self.MODEL.model.fit,
+                                  self.DATASET.train_images,
+                                  self.DATASET.train_labels,
+                                  epochs=self.EPOCHS,
+                                  batch_size=self.BATCH_SIZE,
+                                  validation_data=(self.DATASET.test_images, self.DATASET.test_labels), 
+                                  callbacks=[tensorboard_callback])
+    
+    self._logc.append(_)
 
   def test(self):
 
     if not self.IS_TEST: return
 
-    self.START_TIME = self._time
-    self.Log(self.START_TIME, _T='Start testing:')
-
-    self.RESULT = self.MODEL.model.evaluate(self.DATASET.test_images, self.DATASET.test_labels)
-
-    self.STOP_TIME = self._time
-    self.Log(self.STOP_TIME, _T='Stop testing:')
-    self.TEST_COST_TIME = self._mktime(self.STOP_TIME) - self._mktime(self.START_TIME)
-    self.Log(self.TEST_COST_TIME, _T='Test time (second):')
-    self.Log(self.RESULT[0], _T='total loss:')
-    self.Log(self.RESULT[1], _T='accuracy:')
+    _, result = self._timer.timer('test', self.MODEL.model.evaluate,
+                                  self.DATASET.test_images,
+                                  self.DATASET.test_labels)
+    self._Log(result[0], _T='total loss:')
+    self._Log(result[1], _T='accuracy:')
+    self._logc.append(_)
+    self._logc.append(dict(zip(['test_total_loss', 'test_accuracy'], result)))
 
   def save(self):
 
@@ -244,7 +247,7 @@ class Args:
 
     self.MODEL.model.save(self.SAVE_NAME, include_optimizer=not self.OPT_EXIST)
 
-    self.Log(self.SAVE_NAME, _T='Successfully save model:')
+    self._Log(self.SAVE_NAME, _T='Successfully save model:')
 
   def gimage(self):
 
@@ -258,7 +261,7 @@ class Args:
                to_file=f'{self.H5_NAME}_model.png',
                show_shapes=True)
 
-    self.Log(f'{self.H5_NAME}_model.png', _T='Successfully save model image:')
+    self._Log(f'{self.H5_NAME}_model.png', _T='Successfully save model image:')
   
   def user(self):
     '''user train args'''
@@ -279,4 +282,10 @@ class Args:
 
     self.save()
 
+    from pprint import pprint
+
+    pprint(self._paramc)
+    pprint(self._logc)
+    self._write_config()
+    
 ARGS = Args()
