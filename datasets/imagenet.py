84b9f98e12f410a668661a69686ff1e9ff22e987
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-08 19:11:03 +0800

    Create imagenet.py

---
+++ datasets/imagenet.py
@@ -0,0 +1,24 @@
+from datasets.Dataset import Dataset
+from tensorflow.python.keras import datasets as ds
+
+
+class imagenet(Dataset):
+  """
+    ImageNet 数据集
+  """
+
+  def args(self):
+    self._MISSION_LIST = ['classfication']
+    self.NUM_TRAIN = 60000
+    self.NUM_TEST = 10000
+    self.NUM_VAL = 10000
+    self.NUM_CLASSES = 10
+    self.INPUT_SHAPE = (224, 224, 3)
+    (self.train_x, self.train_y), (self.test_x, self.test_y) = (None, None), (None, None)
+
+
+# test mode
+if __name__ == "__main__":
+  m = imagenet()
+  print(m.ginfo())
+#   print(m.train_x.dtype)
