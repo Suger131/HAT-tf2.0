5a72e31785cf06f896502ed47de2252601c04fb2
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-11 00:10:08 +0800

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,3 +7,5 @@ from datasets.cifar10 import cifar10
 from datasets.fashion_mnist import f_mnist
 from datasets.boston_housing import boston_housing
 from datasets.imagenet import imagenet
+
+from datasets.car10 import car10
