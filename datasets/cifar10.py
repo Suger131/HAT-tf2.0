d2297c0e4ee0c9ba379520d8a743987ab95d7316
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-05-11 15:37:00 +0800

    Update cifar10.py

--- datasets/cifar10.py
+++ datasets/cifar10.py
@@ -13,5 +13,5 @@ class cifar10(Dataset):
     self.NUM_TEST = 10000
     self.NUM_CLASSES = 10
     self.INPUT_SHAPE = (32, 32, 3)
-    (self.train_x, self.train_y), (self.test_x, self.test_y) = ds.cifar10.load_data()
-    self.train_x, self.test_x = self.train_x / 255.0, self.test_x / 255.0
+    (self.train_x, self.train_y), (self.val_x, self.val_y) = ds.cifar10.load_data()
+    self.train_x, self.val_x = self.train_x / 255.0, self.val_x / 255.0
