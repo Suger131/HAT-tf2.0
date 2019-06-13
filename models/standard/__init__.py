11746aef0315b4e5ff677aafcde8961514177745
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-13 17:00:24 +0800

    Update __init__.py

--- models/standard/__init__.py
+++ models/standard/__init__.py
@@ -5,6 +5,7 @@
     Better not change anything.
 """
 
+# pylint: disable=wildcard-import
 
 from hat.models.standard.mlp import mlp
 from hat.models.standard.lenet import lenet
@@ -12,4 +13,5 @@ from hat.models.standard.alexnet import alexnet
 from hat.models.standard.vgg16 import vgg16
 from hat.models.standard.vgg19 import vgg19
 from hat.models.standard.googlenetv4 import googlenetv4
+from hat.models.standard.resnet import *
 
