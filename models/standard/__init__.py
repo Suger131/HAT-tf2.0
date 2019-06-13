97899e065b9841d8de92ae8fe4307e85c6745988
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-13 11:20:36 +0800

    Update __init__.py

--- models/standard/__init__.py
+++ models/standard/__init__.py
@@ -9,3 +9,7 @@
 from models.standard.mlp import mlp
 from models.standard.lenet import lenet
 from models.standard.alexnet import alexnet
+from models.standard.vgg16 import vgg16
+from models.standard.vgg19 import vgg19
+from models.standard.googlenetv4 import googlenetv4
+
