287a1dd40e289821b9b98b077a06a125b3d8a942
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-13 17:36:32 +0800

    Update alexnet.py

--- models/standard/alexnet.py
+++ models/standard/alexnet.py
@@ -1,9 +1,12 @@
 """
   AlexNet 模型
-  For ImageNet
+  本模型默认总参数量[参考基准：cifar10]：
+    Total params:           28,825,320
+    Trainable params:       28,824,616
+    Non-trainable params:   704
   本模型默认总参数量[参考基准：ImageNet]：
-    Total params:           889,351,682
-    Trainable params:       889,350,978
+    Total params:           62,379,752
+    Trainable params:       62,379,048
     Non-trainable params:   704
 """
 
@@ -20,53 +23,36 @@ class alexnet(NetWork, AdvNet):
     AlexNet
   """
   def args(self):
-    self.CONV = [48, 128, 192, 192, 128]
-    self.SIZE = [11, 27, 13, 13, 13]
+    self.CONV = [96, 256, 384, 384, 256]
+    self.SIZE = [11, 5, 3, 3, 3]
+    self.STEP = [4 if self.INPUT_SHAPE[0] >= 160 else 2, 2, 2, 2]
+    self.PAD = 'valid' if self.INPUT_SHAPE[0] >= 160 else 'same'
     self.POOL_SIZE = 3
-    self.POOL_STRIDES = 2
-    self.LOCAL = [2048, 2048, 1000]
+    self.LOCAL = [4096, 4096]
     self.DROP = 0.5
 
   def build_model(self):
+    
     x_in = self.input(self.INPUT_SHAPE)
 
     # conv
-    x1 = x_in
-    x2 = x_in
-    x1 = self.conv(x1, self.CONV[0], self.SIZE[0], activation='relu')
-    x2 = self.conv(x2, self.CONV[0], self.SIZE[0], activation='relu')
-    x1 = self.maxpool(x1, self.POOL_SIZE, self.POOL_STRIDES)
-    x2 = self.maxpool(x2, self.POOL_SIZE, self.POOL_STRIDES)
-    x1 = self.bn(x1)
-    x2 = self.bn(x2)
-    x1 = self.conv(x1, self.CONV[1], self.SIZE[1], activation='relu')
-    x2 = self.conv(x2, self.CONV[1], self.SIZE[1], activation='relu')
-    x1 = self.maxpool(x1, self.POOL_SIZE, self.POOL_STRIDES)
-    x2 = self.maxpool(x2, self.POOL_SIZE, self.POOL_STRIDES)
-    x1 = self.bn(x1)
-    x2 = self.bn(x2)
-    x = self.concat([x1, x2])
-    x1 = self.conv(x, self.CONV[2], self.SIZE[2], activation='relu')
-    x2 = self.conv(x, self.CONV[2], self.SIZE[2], activation='relu')
-    x1 = self.conv(x1, self.CONV[3], self.SIZE[3], activation='relu')
-    x2 = self.conv(x2, self.CONV[3], self.SIZE[3], activation='relu')
-    x1 = self.conv(x1, self.CONV[4], self.SIZE[4], activation='relu')
-    x2 = self.conv(x2, self.CONV[4], self.SIZE[4], activation='relu')
-    x1 = self.maxpool(x1, self.POOL_SIZE, self.POOL_STRIDES)
-    x2 = self.maxpool(x2, self.POOL_SIZE, self.POOL_STRIDES)
-    x = self.concat([x1, x2])
+    x = self.conv(x_in, self.CONV[0], self.SIZE[0], strides=self.STEP[0], padding=self.PAD, activation='relu')
+    x = self.bn(x)
+    x = self.maxpool(x, self.POOL_SIZE, self.STEP[1])
+    x = self.conv(x, self.CONV[1], self.SIZE[1], activation='relu')
+    x = self.bn(x)
+    x = self.maxpool(x, self.POOL_SIZE, self.STEP[2], padding=self.PAD)
+    x = self.conv(x, self.CONV[2], self.SIZE[2], activation='relu')
+    x = self.conv(x, self.CONV[3], self.SIZE[3], activation='relu')
+    x = self.conv(x, self.CONV[4], self.SIZE[4], activation='relu')
+    x = self.maxpool(x, self.POOL_SIZE, self.STEP[3], padding=self.PAD)
 
     # local
     x = self.flatten(x)
-    x1 = self.local(x, self.LOCAL[0])
-    x2 = self.local(x, self.LOCAL[0])
-    x = self.concat([x1, x2])
+    x = self.local(x, self.LOCAL[0])
     x = self.dropout(x, self.DROP)
-    x1 = self.local(x, self.LOCAL[1])
-    x2 = self.local(x, self.LOCAL[1])
-    x = self.concat([x1, x2])
+    x = self.local(x, self.LOCAL[1])
     x = self.dropout(x, self.DROP)
-    x = self.local(x, self.LOCAL[2])
     x = self.local(x, self.NUM_CLASSES, activation='softmax')
 
     self.model = Model(inputs=x_in, outputs=x, name='alexnet')
@@ -74,6 +60,6 @@ class alexnet(NetWork, AdvNet):
 
 # test part
 if __name__ == "__main__":
-  mod = alexnet(DATAINFO={'INPUT_SHAPE': (224, 224 ,3), 'NUM_CLASSES': 10})
+  mod = alexnet(DATAINFO={'INPUT_SHAPE': (32, 32, 3), 'NUM_CLASSES': 1000})
   print(mod.INPUT_SHAPE)
   print(mod.model.summary())
