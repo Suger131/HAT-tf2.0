2b2e85027a009e64a5ca96a4fd02085bdc567395
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-13 17:48:42 +0800

    Update __init__.py

--- models/standard/__init__.py
+++ models/standard/__init__.py
@@ -10,6 +10,7 @@
 from hat.models.standard.mlp import mlp
 from hat.models.standard.lenet import lenet
 from hat.models.standard.alexnet import alexnet
+from hat.models.standard.zfnet import zfnet
 from hat.models.standard.vgg16 import vgg16
 from hat.models.standard.vgg19 import vgg19
 from hat.models.standard.googlenetv4 import googlenetv4
