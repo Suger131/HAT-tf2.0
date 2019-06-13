14653a2ee9ed52867ee786a8854e7e31f282ede4
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-13 21:12:40 +0800

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,35 +3,8 @@
 '''
 
 
-from models.network import NetWork
-from models.advanced import *
+from hat.models.network import *
+from hat.models.advance import *
+from hat.models.utils import *
 
-from models.mlp import mlp
-from models.mlp_r import mlp_r
-from models.lenet import lenet
-from models.alexnet import alexnet
-from models.alexnet_s import alexnet_s
-from models.vgg16 import vgg16
-from models.vgg19 import vgg19
-from models.nvgg16 import nvgg16
-from models.googlenetv4 import googlenetv4
-from models.resnet50 import resnet50
-from models.resnet101 import resnet101
-from models.resnext50 import resnext50
-from models.densenet39 import densenet39
-from models.densenet120 import densenet120
-from models.wrn28_10 import wrn28_10
-from models.mobilenetv2 import mobilenetv2
-
-from models.untitled_3d import untitled_3d
-from models.lenet_3d import lenet_3d
-from models.resnetse29 import resnetse29
-from models.vggse16 import vggse16
-from models.shufflenet import shufflenet
-from models.dwdnet import dwdnet
-
-from models.lenet300 import lenet300
-# from models.tester import tester
-from models.stagenet import stagenet
-from models.enet import enet
-from models.enb0_ci import enb0_ci
+from hat.models.mlp import mlp
