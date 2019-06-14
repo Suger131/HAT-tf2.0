58d7f9fd854067f08c45dda63ed4495da957052e
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-14 15:51:57 +0800

    Update utils.py

--- models/utils.py
+++ models/utils.py
@@ -1,7 +1,15 @@
 import importlib
 
 
-__all__ = ['MLib']
+__all__ = ['MLib', 'NLib']
+
+
+LIB_DICT={
+  'S': 'standard',
+  'A': 'alpha',
+  'B': 'beta',
+  'T': 'test',
+}
 
 
 def MLib(lib=''):
@@ -12,18 +20,22 @@ def MLib(lib=''):
     S: Standard (default)
     A: Alpha
     B: Beta
+    T: Test
     ```
   """
-  if lib == 'S':
-    lib = 'standard'
-  elif lib == 'A':
-    lib = 'alpha'
-  elif lib == 'B':
-    lib = 'beta'
+  if lib in LIB_DICT:
+    lib = LIB_DICT[lib]
   try:
     return importlib.import_module(f'hat.models.{lib}')
   except:
     raise Exception(f'{lib} not in Model Lib')
+
+
+def NLib(lib):
+  if lib in LIB_DICT:
+    lib = LIB_DICT[lib]
+  return lib
+
 
 if __name__ == "__main__":
   models = MLib('S')
@@ -31,4 +43,6 @@ if __name__ == "__main__":
   # lenet = importlib.import_module(f'hat.models.lenet')
   # mod2 = lenet.lenet(DATAINFO={'INPUT_SHAPE': (32, 32, 3), 'NUM_CLASSES': 10})
   # print(dir(lenet))
-  print(getattr(models, '213'))
+  # print(getattr(models, '213'))
+  print(NLib('S'))
+  print('S' in LIB_DICT, 'C' in LIB_DICT)
