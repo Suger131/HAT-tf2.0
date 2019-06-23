0eae723f0d49e762656d0e3e998bf8bd3dbcceff
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-06-23 20:56:23 +0800

    Group Conv 2D update

--- models/advance/groupconv2d.py
+++ models/advance/groupconv2d.py
@@ -27,22 +27,17 @@ class GroupConv(Layer):
       others: The same as normal Convolution.
   """
   def __init__(self, groups, filters=0, kernel_size=1, strides=1, padding='valid',
-               data_format=None, dilation_rate=1, activation=None, use_bias=True, 
-               use_group_bias=False, kernel_initializer='glorot_uniform',
-               bias_initializer='zeros', kernel_regularizer=None,
-               bias_regularizer=None, kernel_constraint=None,
+               data_format=None, activation=None, use_bias=True, use_group_bias=False,
+               kernel_initializer='glorot_uniform', bias_initializer='zeros',
+               kernel_regularizer=None, bias_regularizer=None, kernel_constraint=None,
                bias_constraint=None, **kwargs):
     super().__init__(**kwargs)
-    # rank = 2
     self.groups = groups
     self.filters = filters
-    self.kernel_size = conv_utils.normalize_tuple(
-        kernel_size, 2, 'kernel_size')
+    self.kernel_size = conv_utils.normalize_tuple(kernel_size, 2, 'kernel_size')
     self.strides = conv_utils.normalize_tuple(strides, 2, 'strides') + (1,)
     self.padding = conv_utils.normalize_padding(padding)
     self.data_format = conv_utils.normalize_data_format(data_format)
-    self.dilation_rate = conv_utils.normalize_tuple(
-        dilation_rate, 3, 'dilation_rate')
     self.activation = activations.get(activation)
     self.use_bias = use_bias
     self.use_group_bias = use_group_bias
@@ -64,7 +59,6 @@ class GroupConv(Layer):
                        'should be defined. Found `None`.')
     
     input_dim = int(input_shape[channel_axis])
-    # print(input_shape, input_dim, self.groups)
     assert not input_dim % self.groups
     self.input_dim_i = input_dim // self.groups
     self.output_dim_i = self.filters or self.input_dim_i
@@ -113,58 +107,102 @@ class GroupConv(Layer):
 
   def call(self, inputs, **kwargs):
     
-    _shape = list(K.int_shape(inputs))
+    # Processing shape
+    _shape = inputs.shape.as_list()
     if _shape[0] is None:
-      _shape = _shape[1:]
+      _shape[0] = -1 # batch
     if self.data_format == 'channels_first':
-      channel_axis = 1
-      shape_i = [self.input_dim_i] + _shape[1:3] + [self.groups]
+      _data_format = 'NCHW'
+      shape_i = [_shape[0], self.input_dim_i, _shape[2], _shape[3], self.groups]
     else:
-      channel_axis = -1
-      shape_i = _shape[ :2] + [self.groups, self.input_dim_i]
+      _data_format = 'NHWC'
+      shape_i = [_shape[0], _shape[1], _shape[2], self.groups, self.input_dim_i]
 
-    x = Reshape(shape_i)(inputs)
+    # Reshape to 5D
+    outputs = array_ops.reshape(inputs, shape_i)
     
-    x = self._convolution_op(
-      x,
+    # Convolution
+    outputs = self._convolution_op(
+      outputs,
       self.kernel,
       strides=self.strides,
       padding=self.padding,
       data_format=self.data_format,
     )
 
+    # Group biases adding
+    # NOTE: transpose the G & Ci, and add groups biases, then transpose back
     if self.use_group_bias:
-      pass
-
-    print(x.shape, self.kernel.shape, self.strides)
-    _shape = list(K.int_shape(x))
-    if _shape[0] is None:
-      _shape = _shape[1:]
-    if self.data_format == 'channels_first':
-      shape = [self.output_dim] + _shape[1:3]
-    else:
-      shape = _shape[ :2] + [self.output_dim]
-    x = Reshape(shape)(x)
-    
-    if self.use_bias:
-      
+      print('use g b')
       if self.data_format == 'channels_first':
-        _data_format = 'NCHW'
+        outputs = K.permute_dimensions(outputs, [0, 4, 2, 3, 1])
+        t_shape = outputs.shape.as_list()
+        if t_shape[0] is None:
+          t_shape[0] = -1  # batch
+        outputs = array_ops.reshape(
+          outputs,
+          [t_shape[0], t_shape[1], t_shape[2] * t_shape[3], t_shape[4]]
+        )# reshape to 4D
+        outputs = nn.bias_add(outputs, self.group_biases, data_format=_data_format)
+        outputs = array_ops.reshape(outputs, t_shape)
+        outputs = K.permute_dimensions(outputs, [0, 4, 2, 3, 1])
       else:
-        _data_format = 'NHWC'
-      
-      x = nn.bias_add(x, self.biases, data_format=_data_format)
+        outputs = K.permute_dimensions(outputs, [0, 1, 2, 4, 3])
+        outputs = nn.bias_add(outputs, self.group_biases, data_format=_data_format)
+        outputs = K.permute_dimensions(outputs, [0, 1, 2, 4, 3])
 
+    # Processing shape (back)
+    print(outputs.shape, self.kernel.shape, self.strides)
+    _shape = inputs.shape.as_list()
+    if _shape[0] is None:
+      _shape[0] = -1 # batch
+    if self.data_format == 'channels_first':
+      shape = [_shape[0], self.output_dim, _shape[2], _shape[3]]
+    else:
+      shape = [_shape[0], _shape[1], _shape[2], self.output_dim]
+    
+    # Reshape to 4D
+    outputs = array_ops.reshape(outputs, shape)
+    
+    # Biases Adding
+    if self.use_bias:
+      outputs = nn.bias_add(outputs, self.biases, data_format=_data_format)
+
+    # Activation
     if self.activation is not None:
-      x = self.activation(x)
+      outputs = self.activation(outputs)
 
-    return x
+    return outputs
+
+  def compute_output_shape(self, input_shape):
+    input_shape = tensor_shape.TensorShape(input_shape).as_list()
+
+    if not self.filters:
+      return tensor_shape.TensorShape(input_shape)
+    else:
+      space = input_shape[2:] if self.data_format == 'channels_first' else input_shape[1:-1]
+      new_space = []
+      for i in range(len(space)):
+        new_dim = conv_utils.conv_output_length(
+          space[i],
+          self.kernel_size[i],
+          padding=self.padding,
+          stride=self.strides[i],
+        )
+      new_space.append(new_dim)
+
+      if self.data_format == 'channels_first':
+        return tensor_shape.TensorShape([input_shape[0], self.groups * self.filters] + new_space)
+      else:
+        return tensor_shape.TensorShape([input_shape[0]] + new_space + [self.groups * self.filters])
 
 # test
 if __name__ == '__main__':
   x = K.placeholder((None, 8, 8, 16))
   print(GroupConv(4)(x))
-  print(GroupConv(4, kernel_size=3, activation='relu')(x))
-  print(GroupConv(4, kernel_size=3, strides=2, activation='relu')(x))
-  print(GroupConv(4, kernel_size=3, padding='same', activation='relu')(x))
-  print(GroupConv(4, 8, kernel_size=3, padding='same', activation='relu')(x))
+  # print(GroupConv(4, kernel_size=3, activation='relu')(x))
+  # print(GroupConv(4, kernel_size=3, strides=2, activation='relu')(x))
+  # print(GroupConv(4, kernel_size=3, padding='same', activation='relu')(x))
+  # print(GroupConv(4, 8, kernel_size=3, padding='same', activation='relu')(x))
+  print(GroupConv(8, use_bias=False)(x))
+  print(GroupConv(8, use_bias=False, use_group_bias=True)(x))
