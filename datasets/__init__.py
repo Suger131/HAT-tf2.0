48451591d2c1853745154f4b669f3642ac91db44
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-07-09 11:56:31 +0800

    Add dataset Cifar100

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,6 +4,7 @@
 
 from hat.datasets.mnist import mnist
 from hat.datasets.cifar10 import cifar10
+from hat.datasets.cifar100 import cifar100
 from hat.datasets.fashion_mnist import f_mnist
 from hat.datasets.boston_housing import boston_housing
 from hat.datasets.imagenet import imagenet
---
+++ datasets/cifar100.py
@@ -0,0 +1,17 @@
+from hat.datasets.Dataset import Dataset
+from tensorflow.python.keras import datasets as ds
+
+
+class cifar100(Dataset):
+  """
+  cifar100 数据集
+  """
+
+  def args(self):
+    self._MISSION_LIST = ['classfication']
+    self.NUM_TRAIN = 50000
+    self.NUM_TEST = 10000
+    self.NUM_CLASSES = 100
+    self.INPUT_SHAPE = (32, 32, 3)
+    (self.train_x, self.train_y), (self.val_x, self.val_y) = ds.cifar100.load_data()
+    self.train_x, self.val_x = self.train_x / 255.0, self.val_x / 255.0
