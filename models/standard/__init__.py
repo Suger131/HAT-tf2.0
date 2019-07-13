62f1164e596c67836a051e2c7d3633187466b64f
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-07-13 12:13:19 +0800

    Update vggs

--- models/standard/__init__.py
+++ models/standard/__init__.py
@@ -11,8 +11,7 @@ from hat.models.standard.mlp import mlp
 from hat.models.standard.lenet import lenet
 from hat.models.standard.alexnet import alexnet
 from hat.models.standard.zfnet import zfnet
-from hat.models.standard.vgg16 import vgg16
-from hat.models.standard.vgg19 import vgg19
+from hat.models.standard.vgg import *
 from hat.models.standard.googlenetv4 import googlenetv4
 from hat.models.standard.resnet import *
 from hat.models.standard.densenet import *
