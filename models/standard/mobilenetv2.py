d35e97317dcc34e040f418c5f3780ea064207d5b
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-07-16 19:56:03 +0800

    Update mobilenetv2.py

--- models/standard/mobilenetv2.py
+++ models/standard/mobilenetv2.py
@@ -23,13 +23,11 @@ class mobilenetv2(AdvNet):
   """
   def args(self):
     self.IRB_TIME = [1, 2, 3, 4, 3, 2, 1]
-    self.IRB_CONV = [16, 24, 32, 64, 96, 160, 320]
-    self.IRB_EXPD = [1, 6, 6, 6, 6, 6, 6]
+    self.IRB_CONV = [16, 24, 32, 48, 64, 96, 160]
+    self.IRB_EXPD = [1, 3, 3, 3, 3, 3, 3]
     self.IRB_STEP = [1, 2, 2, 1, 2, 2, 1]
-    self.IRB_CROP = [1, 1, 0, 0, 0, 0, 0]  # 1->True, 0->False
-    self.IRB_CPDN = [0, 0, 1, 0, 1, 1, 0]  # 1->True, 0->False
     
-    self.CONV = [32, 1280]
+    self.CONV = [32, 640]
     
   def build_model(self):
 
@@ -38,8 +36,6 @@ class mobilenetv2(AdvNet):
       self.IRB_CONV,
       self.IRB_EXPD,
       self.IRB_STEP,
-      self.IRB_CROP,
-      self.IRB_CPDN
     ))
 
     x_in = self.input(self.INPUT_SHAPE)
@@ -51,13 +47,14 @@ class mobilenetv2(AdvNet):
 
     x = self.conv_bn(x, self.CONV[1], 1, 1)
     x = self.GAPool(x)
-    x = self.reshape(x, (1, 1, self.CONV[1]))
-    x = self.conv(x, self.NUM_CLASSES, 1, activation='softmax', name='Softmax')  # 加速
-    x = self.flatten(x)
+    x = self.local(x, self.NUM_CLASSES, activation='softmax')
+    # x = self.reshape(x, (1, 1, self.CONV[1]))
+    # x = self.conv(x, self.NUM_CLASSES, 1, activation='softmax', name='Softmax')  # 加速
+    # x = self.flatten(x)
 
     return self.Model(inputs=x_in, outputs=x, name='mobilenetv2')
 
-  def _bottleneck(self, x_in, filters, k, strides=1, first=False, crop=False, crop_down=False):
+  def _bottleneck(self, x_in, filters, k, strides=None):
 
     if K.image_data_format() == 'channels_first':
       axis = 1
@@ -69,33 +66,31 @@ class mobilenetv2(AdvNet):
     x = self.conv(x_in, c, 1)
     x = self.bn(x)
     x = self.relu(x)
-    x = DepthwiseConv2D(3, strides, padding='valid' if crop or crop_down else 'same')(x)
+    x = self.dwconv(x, 3, strides or 1)
     x = self.bn(x)
     x = self.relu(x)
     x = self.conv(x, filters, 1)
     x = self.bn(x)
 
-    if first:
-      x_in = self.conv(x_in, filters, 1, strides)
+    if strides:
+      x_in = self.conv(x_in, filters, 3, strides)
       x_in = self.bn(x_in)
-    if crop or crop_down:
-      _crop = tuple([(int(i / 2), i - int(i / 2)) for i in [_in[j] - _nx[j] for j in (1, 2) for _in, _nx in ((K.int_shape(x_in), K.int_shape(x)),)]])
-      x_in = Cropping2D(cropping=_crop)(x_in)
+
     x = self.add([x_in, x])
     x = self.relu(x)
 
     return x
 
-  def _inverted_residual_block(self, x, n, filters, k, strides=1, crop=False, crop_down=False):
+  def _inverted_residual_block(self, x, n, filters, k, strides=1):
 
-    x = self._bottleneck(x, filters, k, strides, first=True, crop_down=crop_down)
+    x = self._bottleneck(x, filters, k, strides)
 
-    x = self.repeat(self._bottleneck, n - 1, filters, k, crop=crop)(x)
+    x = self.repeat(self._bottleneck, n - 1, filters, k)(x)
 
     return x
 
 
 # test part
 if __name__ == "__main__":
-  mod = mobilenetv2(DATAINFO={'INPUT_SHAPE': (300, 300, 3), 'NUM_CLASSES': 10}, built=True)
+  mod = mobilenetv2(DATAINFO={'INPUT_SHAPE': (100, 100, 3), 'NUM_CLASSES': 114}, built=True)
   mod.summary()
