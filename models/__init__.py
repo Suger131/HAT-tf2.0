d51f8164bbaf2092cf652878383e64fdfc3f2f7f
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-10-13 20:22:26 +0800

    clean

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-  models 子包的init文件
-'''
-
-
-from hat.models.network import *
-from hat.models.advance import *
-from hat.models.utils import *
--- models/beta/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-  Beta Models
-
-"""
-
-
-from hat.models.beta.vss import vss
-from hat.models.beta.dwdsnet import dwdsnet
-from hat.models.beta.dwdsnet2 import dwdsnet2
