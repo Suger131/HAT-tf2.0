36e50f1aef8fc6b100efa5f594236a7497c97484
Suger131 <45256779+Suger131@users.noreply.github.com>
2019-12-10 10:09:10 +0800

    20191210 update 2

---
+++ model/custom/layers/group.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""Squeeze
+
+  File: 
+    /hat/model/custom/layer/squeeze
+
+  Description: 
+    Squeeze transformation Layers
+"""
+
+
+import tensorflow as tf
+
+
+# import setting
+__all__ = [
+  'GroupConv2D',
+]
+
+
+class GroupConv2D(tf.keras.layers.Layer):
+  """GroupConv2D
+  
+    Description:
+      None
+    
+    Attributes:
+      None
+
+    Returns:
+      tf.Tensor
+
+    Raises:
+      None
+
+    Usage:
+      None
+  """
+
+
